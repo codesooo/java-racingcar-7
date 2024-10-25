782cba46fbec50d9eee875ebb0fb40c1fa8259be
codesooo <zlsoo9ee@gmail.com>
2024-10-25 10:34:47 +0900

    Feat : 입력 기능 구현 및 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,27 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.CarFactory;
 import racingcar.input.InputReader;
 import racingcar.output.OutputView;
+import racingcar.util.CarNameParser;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         InputReader inputReader = new InputReader();
         OutputView outputView = new OutputView();
+        CarNameParser carNameParser = new CarNameParser();
 
         // 자동차 이름 입력
         outputView.printCarNamePrompt();
-        inputReader.carNames();
+        String carNamesInput = inputReader.carNames();
+
+        List<String> carNames = carNameParser.parseCarNames(carNamesInput);
+
+        List<Car> cars = CarFactory.createCars(carNames);
+
 
         // 시도 횟수 입력
         outputView.printTryNumPrompt();
--- src/main/java/racingcar/input/InputReader.java
+++ src/main/java/racingcar/input/InputReader.java
@@ -4,10 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputReader {
     public String carNames() {
-        return Console.readLine();
+        String input = Console.readLine();
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력하지 않았습니다.");
+        }
+        return input;
     }
 
     public int numberOfTries() {
-        return Integer.parseInt(Console.readLine());
+        try {
+            String input = Console.readLine();
+            return Integer.parseInt(input); // 숫자 입력 처리
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 숫자를 입력해주세요.");
+        }
     }
 }
